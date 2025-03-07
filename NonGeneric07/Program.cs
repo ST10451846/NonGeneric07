c9fee99a1c1288f1007a3b149831ceec9bcee789
ST10451846 <161336379+ST10451846@users.noreply.github.com>
2025-03-07 14:21:02 +0200

    Add project files.

---
+++ NonGeneric07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGeneric07
+{
+    class NonGenericDictionaryExample
+    {
+        public static void Main()
+        {
+            Hashtable students = new Hashtable();
+            students.Add(1, "John");
+            students.Add(2, "Emma");
+            students.Add(3, 25); // Mixed types allowed
+
+            foreach(DictionaryEntry entry in students)
+            {
+                Console.WriteLine($"ID: {entry.Key}, Name: { entry.Value}");
+            }
+        }
+    }
+}
